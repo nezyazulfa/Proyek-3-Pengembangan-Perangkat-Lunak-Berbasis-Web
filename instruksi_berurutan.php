754efce5e55264cb7aacf9844f0603022f7078c9
Nezya Zulfa F. <nezya.zulfa.tif24@polban.ac.id>
2025-08-29 11:00:27 +0700

    sintaks php

---
+++ instruksi_berurutan.php
@@ -0,0 +1,11 @@
+<?php
+$nama = "Nezya";
+$NIM = "241511085";
+$kelas = "2C";
+$prodi = "D-3 Teknik Informatika";
+
+echo "Nama: $nama<br>";
+echo "NIM: $NIM<br>";
+echo "Kelas: $kelas<br>";
+echo "ProdiL: $prodi<br>";
+?>
---
+++ instruksi_pemilihan.php
@@ -0,0 +1,11 @@
+<?php
+
+$nilai = "90";
+
+if ($nilai<70){
+    echo "Kuliah tidak lulus";
+} else {
+    echo "Kuliah lulus";
+}
+
+?>
