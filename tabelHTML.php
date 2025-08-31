7aca2467594153f13ad177afa8e2f0c402fc5181
Nezya Zulfa F. <nezya.zulfa.tif24@polban.ac.id>
2025-09-01 05:37:39 +0700

    tugas 2

---
+++ tabelHTML.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <table border="1">
+            <tr>
+                <td>nim</td>
+                <td>nama</td>
+                <td>umur</td>
+            </tr>
+            <tr>
+                <td>241511085</td>
+                <td>Nezya Zulfa Fauziah</td>
+                <td>19</td>
+            </tr>
+        </table>
+    </body>
+</html>
---
+++ tabelHTML_looping.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <table border="1">
+            <tr>
+                <td>nim</td>
+                <td>nama</td>
+                <td>umur</td>
+            </tr>
+            <?php
+            $i=1;
+            while($i <=10)
+            {  
+            ?> 
+            <tr>
+                <td>241511085</td>
+                <td>Nezya Zulfa Fauziah</td>
+                <td>19</td>
+            </tr>
+            <?php
+            $i++;
+            }
+            ?>
+        </table>
+    </body>
+</html>
